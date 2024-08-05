abb8e79525e8eaccdd5113ff9e84099bad463d28
Joci <csordas.jozsef97@gmail.com>
2024-08-05 21:06:55 +0200

    x

--- src/components/Expense/Expense.tsx
+++ src/components/Expense/Expense.tsx
@@ -20,56 +20,68 @@ export interface GroupedExpenses {
   id: string;
   category: string;
   items: Expense[];
+  isFavorite: boolean;
   totalAmount: number;
 }
 
 export default function Expense() {
-  const [items, setItems] = useState<Expense[]>([]);
-
-  const itemsByCategory = items.reduce((acc: GroupedExpenses[], item) => {
-    let categoryGroup = acc.find((group) => group.category === item.category);
-
-    if (!categoryGroup) {
-      categoryGroup = {
-        id: item.id,
-        category: item.category,
-        items: [],
-        totalAmount: 0,
-      };
-      acc.push(categoryGroup);
-    }
-
-    categoryGroup.items.push(item);
-    categoryGroup.totalAmount += convertToUsd(item.currency, item.amount);
-
-    categoryGroup.items.sort(
-      (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
-    );
-
-    return acc;
-  }, []);
-
-  const sortedCategories = itemsByCategory.sort((a, b) => {
-    if (a.totalAmount === b.totalAmount) {
-      return b.items.length - a.items.length;
-    } else {
-      return b.totalAmount - a.totalAmount;
-    }
-  });
+  const [items, setItems] = useState<GroupedExpenses[]>([]);
 
   useEffect(() => {
-    setItems(input);
-    console.log(items);
+    const sortedData = getSortedData(input);
+    setItems(sortedData);
   }, []);
+
+  function setLike(id: string) {
+    const updatedData = items.map((item) =>
+      item.id === id ? { ...item, isFavorite: !item.isFavorite } : item
+    );
+
+    setItems(updatedData);
+  }
+
+  function getSortedData(input: Expense[]) {
+    const sorted = input.reduce((acc: GroupedExpenses[], item) => {
+      let categoryGroup = acc.find((group) => group.category === item.category);
+
+      if (!categoryGroup) {
+        categoryGroup = {
+          id: item.id,
+          category: item.category,
+          items: [],
+          isFavorite: false,
+          totalAmount: 0,
+        };
+        acc.push(categoryGroup);
+      }
+
+      categoryGroup.items.push(item);
+      categoryGroup.totalAmount += convertToUsd(item.currency, item.amount);
+
+      categoryGroup.items.sort(
+        (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
+      );
+      console.log("acc", acc);
+      return acc;
+    }, []);
+
+    return sorted.sort((a, b) => {
+      if (a.totalAmount === b.totalAmount) {
+        return b.items.length - a.items.length;
+      } else {
+        return b.totalAmount - a.totalAmount;
+      }
+    });
+  }
 
   return (
     <div className="container">
-      {sortedCategories.map((item) => (
+      {items.map((item) => (
         <Accordion
           key={item.id}
           content={<ExpenseList listItems={item.items} />}
         >
-          <ExpenseDashboard item={item} />
+          <ExpenseDashboard item={item} setLike={setLike} />
         </Accordion>
       ))}
     </div>
--- src/components/Expense/ExpenseDashboard/ExpenseDashboard.tsx
+++ src/components/Expense/ExpenseDashboard/ExpenseDashboard.tsx
@@ -4,11 +4,23 @@ import "./ExpenseDashboard.scss";
 
 interface ExpenseDashboardProps {
   item: GroupedExpenses;
+  setLike(id: string): void;
 }
 
-export default function ExpenseDashboard({ item }: ExpenseDashboardProps) {
+export default function ExpenseDashboard({
+  item,
+  setLike,
+}: ExpenseDashboardProps) {
+  function handleSetFavorite(e) {
+    e.stopPropagation();
+    setLike(item.id);
+  }
+
   return (
     <div className="expense">
+      <div className="expense__icon" onClick={handleSetFavorite}>
+        {item.isFavorite ? "❤️" : "🩶"}
+      </div>
       <div className="expense__category">{item.category}</div>
       <div className="expense__description">
         - {item.items.length} expense(s)
